86ca8ebe3f03ebaab6cadd3ac919568dee15d90e
hiddify <hiddify@gmail.com>
2024-03-17 21:02:13 +0100

    give more memory for windows

--- experimental/libbox/memory.go
+++ experimental/libbox/memory.go
@@ -5,14 +5,18 @@ import (
 	runtimeDebug "runtime/debug"
 
 	"github.com/sagernet/sing-box/common/conntrack"
+	C "github.com/sagernet/sing-box/constant"
 )
 
 func SetMemoryLimit(enabled bool) {
-	const memoryLimit = 45 * 1024 * 1024
-	const memoryLimitGo = memoryLimit / 1.5
+	var memoryLimit uint64 = 45 * 1024 * 1024
+	if !C.IsIos {
+		memoryLimit = 1024 * 1024 * 1024
+	}
+	var memoryLimitGo = memoryLimit * 2 / 3
 	if enabled {
 		runtimeDebug.SetGCPercent(10)
-		runtimeDebug.SetMemoryLimit(memoryLimitGo)
+		runtimeDebug.SetMemoryLimit(int64(memoryLimitGo))
 		conntrack.KillerEnabled = true
 		conntrack.MemoryLimit = memoryLimit
 	} else {
